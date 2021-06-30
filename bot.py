bb1745df4380e567e77d34d27f20a8d50d824475
Telmen <telmenka1@gmail.com>
2021-06-29 23:02:00 -0500

    Great day of work

--- bot.py
+++ bot.py
@@ -2,57 +2,53 @@ import os, discord, random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Retrieve token using dotenv
 load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv("BOT_TOKEN")
 
+# Initialization of client
 intent = discord.Intents(messages = True, reactions = True,\
      guilds = True, members = True, presences = True)
-client = commands.Bot(command_prefix = '!', intents = intent)
-
+client = commands.Bot(command_prefix='!', intents=intent)
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord')
+    print(f"{client.user.name} has connected to Discord")
 
 @client.event
 async def on_member_join(member):
-    guild = discord.utils.get(client.guilds, name = "Orc Pride")
-    channel = discord.utils.get(guild.channels, name = "orc-saloon")
+    guild = client.guilds[0]
+    channel = discord.utils.get(guild.channels, name = "bottest")
     await channel.send(f"{member.name} ч гэнэ үү, {guild.name}-д тавтай морилно уу?\nХужаа л биш бол яахав")
 
-
-@client.command()
+@client.command(help="Creates an instant-invite link")
 async def invite(ctx):
     link = await ctx.channel.create_invite(max_age = 0)
     await ctx.send(f"Here is an instant-invite link: {link}")
 
-@client.command(aliases = ['8ball'], help = "Ask a question into the virtual 8ball")
-async def _8ball(ctx, *, question):
-    responses = ["It is certain",
-        "It is decidedly so",
-        "Without a doubt",
-        "Yes, definitely",
-        "You may rely on it",
-        "As I see it, yes",
-        "Most Likely",
-        "Outlook Good",
-        "Yes",
-        "Signs point to yes",
-        "Reply hazy, try again",
-        "Ask again later",
-        "Better not tell you now",
-        "Cannot predict now",
-        "Concentrate and ask again",
-        "Don't count on it",
-        "My reply is no",
-        "My sources say no",
-        "Outlook not so good", 
-        "Very Doubtful"]
-    
-    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
-
-@client.command()
-async def kick(ctx, member: discord.Member, *, reason = None):
+@client.command(help="Kicks a member from the guild")
+async def kick(ctx, member: discord.Member, *, reason):
+    await member.send(f"You have been kicked for: {reason}")
     await member.kick(reason = reason)
+
+@client.command(help="Bans a member from the guild")
+async def ban(ctx, member: discord.Member):
+    await member.send(f"You have been banned for: {reason}")
+    await member.ban()
+
+@client.command(help="Unbans a user from the guild")
+async def unban(ctx, name, *, reason):
+    guild = client.guilds[0]
+    bans = await guild.bans()
+    user = None
+    for tuuple in bans:
+        if tuuple[1].name == name:
+            user = tuuple[1]
+            await guild.unban(user)
+    return
+
+@client.command(help="Sends a direct message to a member")
+async def dm(ctx, member:discord.Member, *, msg:str):
+    await member.send(msg)
 
 client.run(TOKEN)
